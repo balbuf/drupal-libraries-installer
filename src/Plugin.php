89e6daa60180530b3f416d4b48c609f99d7c9924
Nikolay Lobachev <nikolay.lobachev@gmail.com>
2020-12-17 10:17:55 +0100

    Add abstracted methods deactivate, uninstall required by interface.

--- src/Plugin.php
+++ src/Plugin.php
@@ -67,4 +67,18 @@ class Plugin implements PluginInterface, EventSubscriberInterface {
     }
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  public function deactivate(Composer $composer, IOInterface $io)
+  {
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function uninstall(Composer $composer, IOInterface $io)
+  {
+  }
+
 }
